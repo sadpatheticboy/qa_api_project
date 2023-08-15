34f9c886d54557a6a4d9fbd6dcbf68b910a18bf5
sadpatheticboy <bal-tim@mail.ru>
2023-08-15 13:35:12 +0300

    add funcs put and delete in http_methods.py

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -17,3 +17,13 @@ class HttpMethods:
     def post(url, body):
         result = requests.post(url, json=body, headers=HttpMethods.HEADERS, cookies=HttpMethods.COOKIES)
         return result
+
+    @staticmethod
+    def put(url, body):
+        result = requests.put(url, json=body, headers=HttpMethods.HEADERS, cookies=HttpMethods.COOKIES)
+        return result
+
+    @staticmethod
+    def delete(url, body):
+        result = requests.delete(url, json=body, headers=HttpMethods.HEADERS, cookies=HttpMethods.COOKIES)
+        return result
