e9711bcc457c1f50a052ebb76052e5d8432db539
sadpatheticboy <bal-tim@mail.ru>
2023-08-16 15:55:27 +0300

    add logger

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,3 +1,4 @@
+from utils.logger import Logger
 import requests
 
 
@@ -10,20 +11,28 @@ class HttpMethods:
 
     @staticmethod
     def get(url):
+        Logger.add_request(url, method="GET")
         result = requests.get(url, headers=HttpMethods.HEADERS, cookies=HttpMethods.COOKIES)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def post(url, body):
+        Logger.add_request(url, method="POST")
         result = requests.post(url, json=body, headers=HttpMethods.HEADERS, cookies=HttpMethods.COOKIES)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def put(url, body):
+        Logger.add_request(url, method="PUT")
         result = requests.put(url, json=body, headers=HttpMethods.HEADERS, cookies=HttpMethods.COOKIES)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def delete(url, body):
+        Logger.add_request(url, method="DELETE ")
         result = requests.delete(url, json=body, headers=HttpMethods.HEADERS, cookies=HttpMethods.COOKIES)
+        Logger.add_response(result)
         return result
