2a6686139ea9b27230c0bd5403e0f960f29f8c70
sadpatheticboy <bal-tim@mail.ru>
2023-08-16 10:38:00 +0300

    add method for checking json value by word

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,5 +1,3 @@
-import json
-
 from utils.api import GoogleMapsAPI
 from utils.checking import Checking
 
@@ -16,32 +14,38 @@ class TestCreatePlace:
         place_id = check_post.get("place_id")
         Checking.check_status_code(result_post, 200)
         Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
+        Checking.check_json_value(result_post, "status", "OK")
 
         print("\nGET request (after create):")
         result_get = GoogleMapsAPI.get_new_place(place_id=place_id)
         Checking.check_status_code(result_get, 200)
         Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address',
                                                'types', 'website', 'language'])
+        Checking.check_json_value(result_get, "address", "29, side layout, cohen 09")
 
         print("\nPUT request:")
         result_put = GoogleMapsAPI.put_new_place(place_id=place_id)
         Checking.check_status_code(result_put, 200)
         Checking.check_json_token(result_put, ["msg"])
+        Checking.check_json_value(result_put, "msg", "Address successfully updated")
 
         print("\nGET request (after update):")
         result_get = GoogleMapsAPI.get_new_place(place_id=place_id)
         Checking.check_status_code(result_get, 200)
         Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address',
                                                'types', 'website', 'language'])
+        Checking.check_json_value(result_get, "address", "100 Lenina street, RU")
 
         print("\nDELETE request:")
         result_delete = GoogleMapsAPI.delete_new_place(place_id=place_id)
         Checking.check_status_code(result_delete, 200)
         Checking.check_json_token(result_delete, ["status"])
+        Checking.check_json_value(result_delete, "status", "OK")
 
         print("\nGET request (after delete):")
         result_get = GoogleMapsAPI.get_new_place(place_id=place_id)
         Checking.check_status_code(result_get, 404)
         Checking.check_json_token(result_get, ['msg'])
+        Checking.check_json_value_search(result_get, "msg", "failed")
 
         print("\nTesting of creating, updating and deleting a new location was finished successfully.")
--- utils/checking.py
+++ utils/checking.py
@@ -18,5 +18,23 @@ class Checking:
     def check_json_token(response, expected_value):
         token = json.loads(response.text)
         assert list(token) == expected_value, \
-            f"Incorrect JSON token. Expected: {expected_value} Actual: {list(token)}"
+            f"Incorrect JSON token. Expected: {expected_value}. Actual: {list(token)}"
         print(" - Checking JSON token. Success. All expected fields are present")
+
+    # Метод для проверки значений обязательных полей в ответе запроса
+    @staticmethod
+    def check_json_value(response, field_name, expected_value):
+        value = response.json()
+        check_value = value.get(field_name)
+        assert check_value == expected_value, \
+            f"Incorrect JSON value. Expected: {expected_value}. Actual: {check_value}"
+        print(" - Checking JSON value. Success. Correct value")
+
+    # Метод для проверки значений обязательных полей в ответе запроса по заданному слову
+    @staticmethod
+    def check_json_value_search(response, field_name, search_value):
+        value = response.json()
+        check_value = value.get(field_name)
+        assert search_value in check_value, \
+            f"Incorrect JSON value. Expected: {search_value!r} in {check_value!r}."
+        print(" - Checking JSON value search. Success. Value found")
